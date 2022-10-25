2a436c006ae58bf8ed3321343c3e9042e1e0c73c
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-10-25 12:59:33 +0530

    initial commit

---
+++ src/js/functions.js
@@ -0,0 +1,23 @@
+export function storageAvailable(type) {
+    /**
+     * Check if localstorage is available for use.
+     * Based on: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#feature-detecting_localstorage
+     */
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // e.code replaced with other e.name errors.
+            e.name === 'NotAllowedError' ||
+            e.name === 'UnknownError' ||
+            e.name === 'QuotaExceededError' ||
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            (storage && storage.length !== 0);
+    }
+}
