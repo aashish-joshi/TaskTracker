d40f3eeea768f8d02b46d8976b83e0833569a505
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-12-29 09:36:49 +0530

    optimize
    - re-organize
    - task update function

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,47 +1,66 @@
 import { Task } from "../models/task.js";
 import { sendJsonResponse } from "../common/functions.js";
 
-export const get_all_tasks = (req, res, next) => {
-	Task.find()
-		.select("_id userId name body status createdAt")
-		.sort({ createdAt: -1 })
-		.then((result) => {
-			return sendJsonResponse(req, res, next, 200, result, "task list");
-		})
-		.catch((error) => {
-			console.log(error);
-			return sendJsonResponse( req, res, next, 500, "", "Cannot get data from database" );
-		});
-};
-
-export const get_one_task = (req, res, next) => {
-	Task.findById(req.params.id, "userId name body status createdAt")
-		.then((result) => {
-      return sendJsonResponse( req, res, next, 200, result, "task" );
-		})
-		.catch((error) => {
-			console.log(error);
-			return sendJsonResponse( req, res, next, 500, "", "Cannot get data from database" );
-		});
-};
-
-export const add_new_task = (req, res, next) => {
-	const { body, method } = req;
-
-	if (body && method === "POST") {
-		const task = new Task(req.body);
-		task.save()
+class TaskController {
+	static get_all_tasks = (req, res, next) => {
+		Task.find()
+			.select("_id userId name body status createdAt")
+			.sort({ createdAt: -1 })
 			.then((result) => {
-        return sendJsonResponse( req, res, next, 200, result, "task saved" );
+				return sendJsonResponse(req, res, next, 200, result, "task list");
 			})
 			.catch((error) => {
 				console.log(error);
-				return sendJsonResponse( req, res, next, 500, "", "Cannot add task" );
+				return sendJsonResponse( req, res, next, 500, "", "Cannot get data from database" );
 			});
-	} else {
-		if (method != "POST") {
-			return sendJsonResponse( req, res, next, 405, "", "method not allowed" );
+	};
+	
+	static get_one_task = (req, res, next) => {
+		Task.findById(req.params.id, "userId name body status createdAt")
+			.then((result) => {
+				return sendJsonResponse( req, res, next, 200, result, "task" );
+			})
+			.catch((error) => {
+				console.log(error);
+				return sendJsonResponse( req, res, next, 500, "", "Cannot get data from database" );
+			});
+	};
+	
+	/**
+	 * Add a new task to the database.
+	 * 
+	 * @param {*} req 
+	 * @param {*} res 
+	 * @param {*} next 
+	 * @returns 
+	 */
+	static add_new_task = (req, res, next) => {
+
+		// TODO: Check & verify token in auth header.
+		// TODO: Validate user & append user ID to task
+		const { body, method } = req;
+	
+		if (body && method === "POST") {
+			const task = new Task(req.body);
+			task.save()
+				.then((result) => {
+					return sendJsonResponse( req, res, next, 200, result, "task saved" );
+				})
+				.catch((error) => {
+					console.log(error);
+					return sendJsonResponse( req, res, next, 500, "", "Cannot add task" );
+				});
+		} else {
+			if (method != "POST") {
+				return sendJsonResponse( req, res, next, 405, "", "method not allowed" );
+			}
+			return sendJsonResponse( req, res, next, 400, "", "bad request" );
 		}
-		return sendJsonResponse( req, res, next, 400, "", "bad request" );
+	};
+
+	static update_task = async (req, res, next) => {
+		return sendJsonResponse(req, res, next, 200, {id: req.params.id}, "task to be updated. Not implemented.")
 	}
-};
+}
+
+export { TaskController };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,10 @@
 import Express from "express";
-import {get_all_tasks, get_one_task, add_new_task} from '../controller/taskController.js';
+import { TaskController } from '../controller/taskController.js';
 
 const router = Express.Router();
 
-router.get('/', get_all_tasks);
-router.get('/:id', get_one_task);
-router.post('/', add_new_task);
-
+router.get('/', TaskController.get_all_tasks);
+router.get('/:id', TaskController.get_one_task);
+router.post('/', TaskController.add_new_task);
+router.post('/:id', TaskController.update_task);
 export {router};
