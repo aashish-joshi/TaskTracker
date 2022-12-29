4fbd4b935c5f51237c3569e7b2a2a01139678009
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-12-29 08:34:49 +0530

    fix indent

--- models/task.js
+++ models/task.js
@@ -1,26 +1,29 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const taskSchema = new Schema({
-    userId: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    body: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        default: 'new'
-    },
-    due: {
-        type: Date
-    }
-}, { timestamps: true });
+const taskSchema = new Schema(
+	{
+		userId: {
+			type: String,
+			required: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		body: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			default: "new",
+		},
+		due: {
+			type: Date,
+		},
+	},
+	{ timestamps: true }
+);
 
-export const Task = mongoose.model('Task', taskSchema);
+export const Task = mongoose.model("Task", taskSchema);
--- models/user.js
+++ models/user.js
@@ -1,36 +1,39 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    fname: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    lname: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    status: {
-        type: String,
-        default: 'active',
-        enum: ['inactive','active', 'locked']
-    },
-    password_attempts : {
-        type: String,
-        default: 0
-    }
-}, { timestamps: true });
+const userSchema = new Schema(
+	{
+		fname: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		lname: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			lowercase: true,
+			trim: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		status: {
+			type: String,
+			default: "active",
+			enum: ["inactive", "active", "locked"],
+		},
+		password_attempts: {
+			type: String,
+			default: 0,
+		},
+	},
+	{ timestamps: true }
+);
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
