776cffc27ad8f625aa309669e1ba882e741b8987
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2022-12-29 10:54:58 +0530

    use express-jwt to validate JWTs

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,13 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import Express from "express";
 import { TaskController } from '../controller/taskController.js';
+import { expressjwt } from "express-jwt";
 
 const router = Express.Router();
 
-router.get('/', TaskController.get_all_tasks);
+router.get('/', expressjwt({ secret: process.env.JWT_SECRET, algorithms: [process.env.JWT_ALGO]}), TaskController.get_all_tasks);
 router.get('/:id', TaskController.get_one_task);
 router.post('/', TaskController.add_new_task);
 router.post('/:id', TaskController.update_task);
