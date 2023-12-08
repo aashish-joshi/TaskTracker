2aaa2d781bc2d18551666ff6da49e797a50488e7
aashish-joshi <14803874+aashish-joshi@users.noreply.github.com>
2023-12-08 16:38:30 +0530

    linting and fix default value

--- models/notification.js
+++ models/notification.js
@@ -1,23 +1,23 @@
-import { ObjectID } from "bson";
-import mongoose from "mongoose";
-const { Schema } = mongoose;
+import {ObjectID} from 'bson';
+import mongoose from 'mongoose';
+const {Schema} = mongoose;
 
 const notificationSchema = new Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		templateId: {
-			type: String,
-			required: true,
-		},
-		from: {
-			type: String,
-			default: "aashish@aashishjoshi.co.in",
-		},
-	},
-	{ timestamps: true }
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      templateId: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: String,
+        required: true,
+      },
+    },
+    {timestamps: true},
 );
 
-export const Task = mongoose.model("Notification", notificationSchema);
+export const Task = mongoose.model('Notification', notificationSchema);
